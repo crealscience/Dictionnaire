455e110f308ea42a34990ad73ce7362ea6b25667
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:51:54 +0200

    Create indexFilAriane.php

---
+++ indexPhp/indexFilAriane.php
@@ -0,0 +1,22 @@
+<!-- XLS, 2017 -->
+<script>
+        document.onreadystatechange = function () {
+        if(
+    document.readyState === "complete"
+  ){
+     
+        if (
+      document.getElementById(
+        "ariane" 
+      )
+    ){
+        var t = document.getElementById(
+        'ariane'
+      ).innerHTML;
+        document.getElementById(
+        'arianeBox'
+      ).innerHTML = t;
+        }
+        }
+        }
+</script>
