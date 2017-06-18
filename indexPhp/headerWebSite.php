2bbfe5b8180271efdbeca7cb26da61508f946c89
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:50:03 +0200

    Create headerWebSite.php
    headers

---
+++ indexPhp/headerWebSite.php
@@ -0,0 +1,12 @@
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <meta name="keywords" content="ontologie, dictionnaire, sciences, moyen âge, médiéval, botanique, zoologie, pharmacopée, chirurgie, phytographie, ancien français, xml, xquery, magie, diététique, anatomie" />
+ <script type="text/javascript" src="http://www.crealscience.fr/JS/ie_xml.js"></script>
+ <script type="text/javascript" src="http://www.crealscience.fr/JS/js.js"></script>
+ <link rel="stylesheet" type="text/css" title="base" media="screen" href="http://www.crealscience.fr/CSS/dicobase.css"/>
+ <link rel="stylesheet" type="text/css" title="base" media="screen" href="http://www.crealscience.fr/CSS/dicoreq.css"/>
+ <link rel="stylesheet" type="text/css" title="base" media="screen" href="http://www.crealscience.fr/CSS/dicodefxml.css"/>
+ <link rel="icon" type="image/png" href="<?echo $logos[1];?>" />
+ <link href='http://fonts.googleapis.com/css?family=Source Sans Pro|Syncopate|Belleza|Nixie+One|Open+Sans|PT+Serif+Caption|Eagle+Lake&subset=latin,latin-ext' rel='stylesheet' type='text/css'> 
+ <link rel=”alternate” type=”application/rss+xml” title=”RSS” href=”/var/www/vhosts/i-def.fr/httpdocs/rss/rss.xml″ />
+
+
