137966007fc5d635113bf7cb9ce0a99188e45e96
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:53:38 +0200

    Create indexPhploginClass.php
    Boîtier de connexion (c) Design XLS (création graphique JS --> tumult.com/hype/)

---
+++ indexPhploginClass.php
@@ -0,0 +1,4 @@
+<!-- Boîtier de connexion -->
+<div id="creallogin_hype_container" style="margin:auto;margin-top: 25px;position:relative;width:188px;height:224px;overflow:hidden;position:fixed; top: 30%;left:30%;z-index:5000;display:none;" aria-live="polite">
+ <script type="text/javascript" charset="utf-8" src="http://www.crealscience.fr/crealLogin/crealLogin.hyperesources/creallogin_hype_generated_script.js?54271"></script>
+</div>
