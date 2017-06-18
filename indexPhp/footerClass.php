8f34e144fe63f75d4e268edecf78fd4dede4d118
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:52:12 +0200

    Create footerClass.php

---
+++ indexPhp/footerClass.php
@@ -0,0 +1,2 @@
+<div id="footerIdefx" style="z-index: 7000;width:100%; position:fixed; bottom:0px; left: 0px; text-align: center; color: #A0A0A0; font-size: xx-small; background-color:white; padding: 3px;">Edition numérique: Xavier-Laurent Salvador</div>
+<a style="display:none;" href="https://plus.google.com/103783772087446954534" rel="publisher">Google+</a>
