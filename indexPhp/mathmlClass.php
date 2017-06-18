f6812491f78a4594b39a9831ff1e5a75d9c5b7f2
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:54:32 +0200

    Create mathmlClass.php
    MathML

---
+++ indexPhp/mathmlClass.php
@@ -0,0 +1,15 @@
+
+<script type="text/x-mathjax-config">
+           MathJax.Hub.Config(
+  {
+           MMLorHTML: {
+       prefer: {
+         Firefox: "MML" 
+      } 
+    }
+          }
+);
+         </script>
+        <script type="text/javascript"
+         src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
