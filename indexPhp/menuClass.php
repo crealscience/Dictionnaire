6470a190f2664abc2b4e72bba7fd79dac5cf5b4c
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:56:48 +0200

    Create menuClass.php
    Affichage du menu sur le côté Gauche et des panneaux publicitaires

---
+++ indexPhp/menuClass.php
@@ -0,0 +1,8 @@
+<!-- XLS 2017 -->
+<ul id="menucote">
+        <? 	AffichMenuGauche($BoutonGauche[$_SESSION['langue']]);?>
+ </ul>
+ <div id="crealpub_hype_container" style="margin:auto;position:relative;width:80px;height:125px;overflow:hidden;background-color: white; border-radius:20px;border: 6px solid #D9CEAC; display:block; clear:both;" aria-live="polite">
+    <script type="text/javascript" charset="utf-8" src="http://www.crealscience.fr/crealPub/crealPub.hyperesources/crealpub_hype_generated_script.js?73963"></script>
+</div>
+
