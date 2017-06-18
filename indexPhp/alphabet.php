9794024e28371844ccc2840db0fb5780ac24ea15
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:43:54 +0200

    Create alphabet.php

---
+++ indexPhp/alphabet.php
@@ -0,0 +1,20 @@
+<form name="alphabet" action="<?echo $_SERVER["PHP_SELF"].'?langue='.$_SESSION['langue'];?>" method="post">
+       <input type="hidden" name="indexlettreonglet" value="">
+        <?
+                if (
+  $_SESSION['groupe'] == "visiteur"
+) {
+  echo '<div id="xx">Cher Visiteur, cliquez ici la lettre </div id="xx">';
+}
+		echo $page->createAlphabet(
+  'OK'
+);
+                if (
+  $_SESSION['groupe'] == "visiteur"
+) {
+  echo '<div id="xx"> (
+    en cours
+  ). Les autres lettres seront prochainement accessibles.</div>';
+}
+        ?>
+</form>
