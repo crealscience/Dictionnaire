2a1403540913241608029c82106cd37b696884c7
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:48:26 +0200

    Create feuillure.php

---
+++ indexPhp/feuillure.php
@@ -0,0 +1,7 @@
+<!-- Xavier-Laurent Salvador, 2017 -->
+<!-- La feuille: copyrignt design Xavier-Laurent Salvador -->
+<!-- Logiciel de design Hype: tumult.com/hype/ -->
+
+<div id="crealfeuillure_hype_container" style="margin:auto;position:fixed;right:0px; top:0px; z-index:-1; width:87px;height:111px;overflow:hidden;" aria-live="polite">
+<script type="text/javascript" charset="utf-8" src="http://www.crealscience.fr/crealFeuillure/crealfeuillure.hyperesources/crealfeuillure_hype_generated_script.js?14254"></script>
+</div>
