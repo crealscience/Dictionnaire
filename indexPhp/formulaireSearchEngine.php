30d74318221985506da8a87926bb2726da915638
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:49:11 +0200

    Create formulaireSearchEngine.php

---
+++ indexPhp/formulaireSearchEngine.php
@@ -0,0 +1,5 @@
+<!-- Xavier-Laurent Salvador, 2017 -->
+<form id="formmot" action="http://www.crealscience.fr/index.php?langue='fr'" method="get">
+<input class="bg_form" id="inputmot" type="text" name="mot" onkeyup=<?echo $onkeyup;?> value="<?echo $mot;?>" onfocus=<?echo $onfocus;?> />
+<input type="hidden" name="proeditione" value="<?echo $mot;?>" />
+</form>
