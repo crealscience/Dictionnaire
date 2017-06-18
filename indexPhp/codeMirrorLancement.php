b82c6580c604815023a5c60c298a425c784d97c0
crealscience <xavierlaurentsalvador+gitCreal@gmail.com>
2017-06-18 10:46:51 +0200

    Create codeMirrorLancement.php

---
+++ indexPhp/codeMirrorLancement.php
@@ -0,0 +1,8 @@
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/codemirror/3.19.0/codemirror.js"></script>
+<link rel=stylesheet href="codemirror/doc/docs.css">
+<link rel="stylesheet" href="codemirror/lib/codemirror.css">
+<script src="codemirror/lib/codemirror.js"></script>
+<script src="codemirror/mode/xml/xml.js"></script>
+<script src="codemirror//addon/hint/show-hint.js"></script>
+<script src="codemirror//addon/hint/xml-hint.js"></script>
